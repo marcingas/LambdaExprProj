d23df875d22a49d91250864e1f61aedc64db49d1
Marcin <gasiorek.mail@gmail.com>
2023-03-28 10:51:16 +0200

    functions

--- src/functionalInterfaceLearn/Runner2.java
+++ src/functionalInterfaceLearn/Runner2.java
@@ -2,7 +2,11 @@ package functionalInterfaceLearn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.IntPredicate;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Runner2 {
     public static void main(String[] args) {
@@ -24,38 +28,95 @@ public class Runner2 {
         employees.add(jonaat);
         employees.add(snowmen);
 
-        System.out.println("Employees over 30: ");
-        System.out.println("normal loop version_____________");
-        for (Employee2 employee2 : employees) {
-            if (employee2.getAge() > 30) {
-                System.out.println(employee2.getName());
+
+//        System.out.println("Employees over 30: ");
+//        System.out.println("normal loop version_____________");
+//        for (Employee2 employee2 : employees) {
+//            if (employee2.getAge() > 30) {
+//                System.out.println(employee2.getName());
+//            }
+//        }
+//        System.out.println("lambda version: ------------");
+//        employees.forEach(employee2 -> {
+//            if(employee2.getAge()>30){
+//                System.out.println(employee2.getName());
+//            }
+//        });
+//        System.out.println("Employees<=30----------------");
+//        employees.forEach(employee2 -> {
+//            if(employee2.getAge()<=30){
+//                System.out.println(employee2.getName() + ":" + employee2.getAge());
+//            }
+//        });
+//
+//        printEmployeesByAge(employees,"Employee over 30", employee2 -> employee2.getAge() > 30);
+//        printEmployeesByAge(employees,"\n Employee less or equal then 30", employee2 -> employee2.getAge() <= 30);
+//        printEmployeesByAge(employees, "\n employees younger then 25 ", new Predicate<Employee2>() {
+//            @Override
+//            public boolean test(Employee2 employee2) {
+//                return employee2.getAge()<25;
+//            }
+//        });
+//
+//        IntPredicate greaterThen15 = i -> i > 15;
+//        IntPredicate lessThan100 = i -> i < 100;
+//        System.out.println("chaining IntPredicates " + greaterThen15.and(lessThan100).test(12));
+//        System.out.println(greaterThen15.test(10));
+//        int a = 20;
+//        System.out.println(greaterThen15.test(a + 5));
+//
+//        //supplier interface:
+//        Random random = new Random();
+//        Supplier<Integer> randomSupplier = ()-> random.nextInt(2000) - 1000;
+//        for(int i = 0;i < 10; i++){
+//            System.out.println(randomSupplier.get());
+//        }
+//
+//        employees.forEach(employee2 -> {
+//            String lastName = employee2.getName().substring(employee2.getName().indexOf(' ') + 1);
+//            System.out.println("Last name is: " + lastName);
+//        });
+//    }
+        Function<Employee2, String> getLastName = (Employee2 employee2) -> {
+            return employee2.getName().substring(employee2.getName().indexOf(' ') + 1);
+        };
+
+        String lastName = getLastName.apply(employees.get(2));
+//        System.out.println(lastName);
+
+        Function<Employee2, String> getFirstName = (Employee2 employee2) -> {
+            return employee2.getName().substring(0, employee2.getName().indexOf(' '));
+        };
+        String firstName = getFirstName.apply(employees.get(2));
+//        System.out.println(firstName);
+//        use getName method:
+        Random random1 = new Random();
+        for(Employee2 employee2: employees){
+            if(random1.nextBoolean()){
+                System.out.println("first Name: " + getName(getFirstName,employee2));
+            }else{
+                System.out.println("last name: " + getName(getLastName,employee2));
             }
         }
-        System.out.println("lambda version: ------------");
-        employees.forEach(employee2 -> {
-            if(employee2.getAge()>30){
-                System.out.println(employee2.getName());
-            }
-        });
-        System.out.println("Employees<=30----------------");
-        employees.forEach(employee2 -> {
-            if(employee2.getAge()<=30){
-                System.out.println(employee2.getName() + ":" + employee2.getAge());
-            }
-        });
-
-        printEmployeesByAge(employees,"Employee over 30", employee2 -> employee2.getAge() > 30);
-        printEmployeesByAge(employees,"Employee less or equal then 30", employee2 -> employee2.getAge() <= 30);
 
 
     }
     private static void printEmployeesByAge(List<Employee2>employees, String ageText, Predicate<Employee2>ageCondition){
         System.out.println(ageText);
         System.out.println("========================");
-       for (Employee2 employee2 : employees){
-           if(ageCondition.test(employee2)){
-               System.out.println(employee2.getName() + " : " + employee2.getAge());
-           }
-       }
+        for (Employee2 employee2 : employees){
+            if(ageCondition.test(employee2)){
+                System.out.println(employee2.getName() + " : " + employee2.getAge());
+            }
+        }
+
+
+    }
+    private static String getName(Function<Employee2,String> getName, Employee2 employee2){
+        return getName.apply(employee2);
     }
 }
+
+
+
+
